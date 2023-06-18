a3e38925d966fcfb69b91fd34c9c6d21ae1c9a8c
ilkin1Zahidov <ilkinzahidov01@gmail.com>
2023-06-18 22:38:53 +0400

    fixed all

--- src/components/Chat/Chats.jsx
+++ src/components/Chat/Chats.jsx
@@ -5,7 +5,7 @@ import { ChatContext } from "../../context/ChatContext";
 import { db } from "../../firebase/ChatFirebase";
 
 const Chats = () => {
-  const [chats, setChats] = useState([]);
+  const [chats, setChats] = useState({});
 
   const { currentUser } = useContext(AuthContext);
   const { dispatch } = useContext(ChatContext);
@@ -30,16 +30,16 @@ const Chats = () => {
   
   return (
     <div className="chats">
-      {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
+      {Object.entries(chats)?.sort((a, b) => b[1].date - a[1].date).map((chat) => (
         <div
           className="userChat"
           key={chat[0]}
-          onClick={() => handleSelect(chat[1].userInfo)}
+          onClick={() => handleSelect(chat[1]?.userInfo)}
         >
-          <img src={chat[1].userInfo.photoURL} alt="" />
+          {chat[1]?.userInfo?.photoURL && <img src={chat[1]?.userInfo.photoURL} alt="" />}
           <div className="userChatInfo">
-            <span>{chat[1].userInfo.displayName}</span>
-            <p>{chat[1].lastMessage?.text}</p>
+            <span>{chat[1]?.userInfo?.displayName}</span>
+            <p>{chat[1]?.lastMessage?.text}</p>
           </div>
         </div>
       ))}
--- src/components/Email/EmailSideBar.jsx
+++ src/components/Email/EmailSideBar.jsx
@@ -1,6 +1,5 @@
-import React, { useContext, useEffect, useState } from "react";
+import React from "react";
 import { Button } from "@mui/material";
-import { doc, onSnapshot } from "firebase/firestore";
 import { useDispatch } from "react-redux";
 import "../../assets/css/emailSideBar.css";
 import SidebarOption from "./SidebarOption";
@@ -11,47 +10,19 @@ import MarkEmailReadIcon from "@mui/icons-material/MarkEmailRead";
 import NoteIcon from "@mui/icons-material/Note";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { openSendMessage } from "../../control/mailSlice";
-import { AuthContext } from "../../context/AuthContext";
-import { ChatContext } from "../../context/ChatContext";
-import { db } from "../../firebase/EmailFireBase";
 import { useNavigate } from "react-router-dom";
 import { Box, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
+import Chats from "../Chat/Chats";
 
 function EmailSideBar() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-
   const dispatch = useDispatch();
-  const [chats, setChats] = useState([]);
-  const { currentUser } = useContext(AuthContext);
-  const { dispatch: chatDispatch } = useContext(ChatContext);
-
   const navigate= useNavigate()
-useEffect(() => {
-  const getChats = () => {
-    if (currentUser) {
-      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
-        setChats(doc.data());
-      });
-      return () => {
-        unsub();
-      };
-    }
-  };
-
-  getChats();
-}, [currentUser]);
-
-  const handleSelect = (u) => {
-    chatDispatch({ type: "CHANGE_USER", payload: u });
-  };
-
   const StarMail = () => {
     navigate("/StarMail")
   }
-
-  
 
   return (
     <div className="emailSideBar" >
@@ -89,21 +60,7 @@ useEffect(() => {
 
         <div className="sidebar_footer">
           <div className="chats">
-            {Object.entries(chats)
-              ?.sort((a, b) => b[1].date - a[1].date)
-              .map((chat) => (
-                <div
-                  className="userChat"
-                  key={chat[0]}
-                  onClick={() => handleSelect(chat[1].userInfo)}
-                >
-                  <img src={chat[1].userInfo.photoURL} alt="" />
-                  <div className="userChatInfo">
-                    <span>{chat[1].userInfo.displayName}</span>
-                    <p>{chat[1].lastMessage?.text}</p>
-                  </div>
-                </div>
-              ))}
+            <Chats/>
           </div>
         </div>
       </div>
